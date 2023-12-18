9a46c631601db5ea346d507e7905198c94d3bfe6
Simon <26143030+SimonKolsch@users.noreply.github.com>
2023-12-18 22:05:45 +0100

    * adding datatable * adding form * refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
-import '@mantine/core/styles.css';
-import './App.css'
+
 import {MantineProvider} from "@mantine/core";
 import {RouterProvider} from "react-router-dom";
 import {router} from "./setup";
 
+import '@mantine/core/styles.layer.css';
+import 'mantine-datatable/styles.layer.css';
+
+import './layout.css'
+
 function App() {
 
   return (
-    <MantineProvider>
+    <MantineProvider defaultColorScheme="auto">
       <RouterProvider router={router} />
     </MantineProvider>
   )
--- frontend/src/pages/accounting/accountOverview.tsx
+++ frontend/src/pages/accounting/accountOverview.tsx
@@ -1,15 +1,66 @@
 import React from "react";
 import {useLoaderData} from "react-router-dom";
 import {Account} from "../../common/types";
+import { DataTable } from 'mantine-datatable';
+import { useForm } from "@mantine/form";
+import {Button, Group, TextInput } from "@mantine/core";
 
 export const AccountOverview: React.FC = () => {
   const data: Account[] = useLoaderData() as Account[];
-  const listItems = data.map((account) =>
-      <li key={account.id}>
-        {account.auditInfo.lastModifiedBy}
-      </li>
-  );
+  const form = useForm({
+    initialValues: {
+      iban: '',
+      owner: 1,
+    },
+
+    validate: {
+      iban: (value) => (value.length == 22 ? null : 'Iban should have a length of 22 '),
+    },
+  });
+
+  const submit = (values :{iban: string, owner: number}) => {
+    console.log(values)
+  }
+
   return (
-      <ul>{listItems}</ul>
-  );
+      <>
+      <form onSubmit={form.onSubmit((values) => submit(values))}>
+        <TextInput
+            withAsterisk
+            label="iban"
+            placeholder="your Iban"
+            {...form.getInputProps('iban')}
+        />
+
+        <Group justify="flex-end" mt="md">
+          <Button type="reset" onClick={() => form.reset()}>Reset</Button>
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
+  <DataTable
+      withTableBorder
+      borderRadius="sm"
+      withColumnBorders
+      striped
+      highlightOnHover
+      // provide data
+      records={data}
+      // define columns
+      columns={[
+        {
+          accessor: 'id',
+          // this column has a custom title
+          title: '#',
+          // right-align column
+          textAlign: 'right',
+        },
+        {accessor: 'iban'},
+        {
+          accessor: 'owner',
+        },
+      ]}
+  />
+</>
+)
+  ;
 };
